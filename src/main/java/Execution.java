deafd9cf890dfde7da51530094851e4f1f68b6f8
Manish Verma <manish.verma@olacabs.com>
2018-02-27 20:02:14 +0530

    [ManishVerma]Added one sout

--- src/main/java/Execution.java
+++ src/main/java/Execution.java
@@ -10,7 +10,8 @@ public class Execution extends AllActions {
     public static void main(String[] args) {
         CustomResponse customResponse = getApiResponse();
         if (customResponse == null) {
-            System.exit(-1);
+            System.out.println("No Response from API. Termination Program!");
+            System.exit(0);
         }
         List<Result> allResult = customResponse.getRestResponse().getResult();
         System.out.println("Starting Execution ! Type Stop To Terminate Program !");
